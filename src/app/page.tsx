2e74f4c7456ed2d7ffc736f7de97c12892a97417
israaQaba <israa.qaba98@gmail.com>
2025-05-14 13:53:06 +0300

    safety icin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ import emailjs from 'emailjs-com';
 export default function HomePage() {
   const [messageSent, setMessageSent] = useState(false);
 
-  const sendEmail = (values: any) => {
-    const serviceID = 'service_ja1yuud';
-    const templateID = 'template_yrnu5rs';
-    const publicKey = 'J5EPnavnPJidmo_ks';
+  const sendEmail = (values: { name: string; surname: string; email: string; country: string; dob: string; message: string }) => {
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
+    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!;
     return emailjs.send(serviceID, templateID, values, publicKey);
   };
 
